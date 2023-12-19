089d0b66b1bdc4e69e8f2db7613778b5e72413d0
mostafa-de <mostafafayyado1@gmail.com>
2023-12-20 02:16:21 +0300

    init deploy 6

--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.upload_image, name='home'),
     re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
